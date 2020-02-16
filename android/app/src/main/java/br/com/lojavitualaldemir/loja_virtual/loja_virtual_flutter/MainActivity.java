ad96d8b697fb7e8e2c5d540b649c26435c683305
aldemir.gomesD <aldemir.gomes@aftisolucoes.com.br>
2020-02-16 18:54:30 -0300

    first commit

---
+++ android/app/src/main/java/br/com/lojavitualaldemir/loja_virtual/loja_virtual_flutter/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.lojavitualaldemir.loja_virtual.loja_virtual_flutter;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
